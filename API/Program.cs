ed73aae6270d0c42c4f6f91f942714b76dd9d25a
Jonatan Em <jonatan.mikolajczyk132@gmail.com>
2020-09-13 17:50:15 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -2,25 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
+      IHost host = CreateHostBuilder(args).Build();
+      using (IServiceScope scope = host.Services.CreateScope())
+      {
+        IServiceProvider services = scope.ServiceProvider;
+        ILoggerFactory loggerFactory = services.GetRequiredService<ILoggerFactory>();
+        try
         {
-            CreateHostBuilder(args).Build().Run();
+          StoreContext context = services.GetRequiredService<StoreContext>();
+          await context.Database.MigrateAsync();
+          await StoreContextSeed.SeedAsync(context, loggerFactory);
         }
+        catch (Exception ex)
+        {
+          ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+          logger.LogError(ex, "An error occured during migration");
+        }
+      }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
