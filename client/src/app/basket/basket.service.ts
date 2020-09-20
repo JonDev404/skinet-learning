4831d5e4df06bdd8212b95c3eaadd6a2015dd7ee
Jon Em <jonatan.mikolajczyk132@gmail.com>
2020-09-20 18:26:39 +0200

    End of section 14

---
+++ client/src/app/basket/basket.service.ts
@@ -0,0 +1,151 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import {
+  Basket,
+  IBasket,
+  IBasketItem,
+  IBasketTotals,
+} from '../shared/models/basket';
+import { IProduct } from '../shared/models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BasketService {
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
+  baseUrl = environment.apiUrl;
+  basket$ = this.basketSource.asObservable();
+  basketTotal$ = this.basketTotalSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getBasket(id: string): Observable<void> {
+    return this.http.get<IBasket>(`${this.baseUrl}basket?id=${id}`).pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+        this.calculateTotals();
+      })
+    );
+  }
+
+  setBasket(basket: IBasket): void {
+    this.http.post(`${this.baseUrl}basket`, basket).subscribe(
+      (response: IBasket) => {
+        this.calculateTotals();
+
+        this.basketSource.next(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getCurrentBasketValue(): IBasket {
+    return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1): void {
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(
+      item,
+      quantity
+    );
+
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  incrementItemQuantity(item: IBasketItem): void {
+    const basket = this.getCurrentBasketValue();
+    const foundItem = basket.items.find((i) => i.id === item.id);
+    foundItem.quantity++;
+    this.setBasket(basket);
+  }
+
+  decrementItemQuantity(item: IBasketItem): void {
+    const basket = this.getCurrentBasketValue();
+    const foundItem = basket.items.find((i) => i.id === item.id);
+    if (foundItem.quantity > 1) {
+      foundItem.quantity--;
+      this.setBasket(basket);
+    } else {
+      this.removeItemFromBasket(foundItem);
+    }
+  }
+
+  removeItemFromBasket(item: IBasketItem): void {
+    const basket = this.getCurrentBasketValue();
+    if (basket.items.some((i) => i.id === item.id)) {
+      basket.items = basket.items.filter((i) => i.id !== item.id);
+      if (basket.items.length > 0) {
+        this.setBasket(basket);
+      } else {
+        this.deleteBasket(basket);
+      }
+    }
+  }
+
+  deleteBasket(basket: IBasket): void {
+    this.http.delete(`${this.baseUrl}basket?id=${basket.id}`).subscribe(
+      () => {
+        this.basketSource.next(null);
+        this.basketTotalSource.next(null);
+        localStorage.removeItem('basket_id');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private calculateTotals(): void {
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a, b) => b.price * b.quantity + a, 0);
+    const total = subtotal + shipping;
+    this.basketTotalSource.next({ shipping, total, subtotal });
+  }
+
+  private addOrUpdateItem(
+    items: IBasketItem[],
+    itemToAdd: IBasketItem,
+    quantity: number
+  ): IBasketItem[] {
+    const item = items.find((i) => i.id === itemToAdd.id);
+
+    if (!item) {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      item.quantity += quantity;
+    }
+
+    return items;
+  }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(
+    item: IProduct,
+    quantity: number
+  ): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType,
+    };
+  }
+}
